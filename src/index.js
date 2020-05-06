1730a00bf490b352aa7e0e6d8aabead9f950446f
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-05-06 20:53:45 +0530

    :sparkles: button and slider design with jquery

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-$("document").ready(function() {
-    alert("Hello World!")
+// add toggle class to switch
+
+let $btn = $(".toggle-btn");
+
+$btn.each(function() {
+    $(this).click(function() {
+        $(this).toggleClass("toggle-btn--active")
+    })
 })
+
+// add slider functionality
+
+let $slider = $(".range")
+
+$slider.each(function() {
+    let $prev = $(this).prev();
+
+    $(this).change(function() {
+        $prev.html(`<p>${this.value}</p>`);
+    })
+    $(this).mousemove(function() {
+        $prev.html(`<p>${this.value}</p>`);
+    })
+})
+
