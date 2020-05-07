88683c8dab65c90b3fc0ad3140b1f0a7a97df070
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-05-07 20:00:15 +0530

    :sparkles: dynamic styling of images

--- src/index.js
+++ src/index.js
@@ -40,3 +40,150 @@ $btn.each(function() {
 })
 
 
+// dynamic styling
+// these are called design elements
+
+let star = {
+    blur: "",
+    brightness: "",
+    contrast: "",
+    grayscale: "",
+    hue: "",
+    invert: "",
+    opacity: "",
+    saturate: "",
+    sepia: ""
+}
+
+$btn.each(function() {
+    let $ele = $(this); // current button
+    
+    let prop = $ele.next().text().trim()
+    let $value = $ele.parent().next().find(".range__content")
+    let num = $value.children().first().text()
+    let unit = $value.children().last().text()
+
+    // jquery object holding current slider
+    let $slide = $ele.parent().next().find(".range")
+
+    // native DOM object is made from jquery object for functionality
+    // of mouseover (coz, mouseover changed values without slider thumb moving otherwise)
+    let slideDOM = $slide.get();
+
+    // gives value to those global design elements
+    let eachStyle = function() {
+        switch (prop) {
+            case "blur":
+                star.blur = `blur(${num}${unit})`
+                break;
+            case "brightness":
+                star.brightness = `brightness(${num}${unit})`
+                break;
+            case "contrast":
+                star.contrast = `contrast(${num}${unit})`
+                break;
+            case "grayscale":
+                star.grayscale = `grayscale(${num}${unit})`
+                break;
+            case "hue-rotate":
+                star.hue = `hue-rotate(${num}${unit})`
+                break;
+            case "invert":
+                star.invert = `invert(${num}${unit})`
+                break;
+            case "opacity":
+                star.opacity = `opacity(${num}${unit})`
+                break;
+            case "saturate":
+                star.saturate = `saturate(${num}${unit})`
+                break;
+            case "sepia":
+                star.sepia = `sepia(${num}${unit})`
+                break;
+        }
+    }
+
+    let allStyles = function() {
+        let design = `${star.blur} ${star.brightness} ${star.contrast} ${star.grayscale} ${star.hue} ${star.invert} ${star.opacity} ${star.saturate} ${star.sepia}`
+        return design;
+    }
+ 
+    $ele.click(function() {   
+        if ($ele.hasClass("toggle-btn--active")) {
+            let design;
+            
+            $(slideDOM).change(function() {
+                num = this.value;
+                eachStyle();
+                design = allStyles();
+                $("#edit").css("filter", design)
+            })
+            $(slideDOM).mousemove(function() {
+                num = this.value;
+                eachStyle();
+                design = allStyles();
+                $("#edit").css("filter", design)
+            })
+        } else {
+            let style = $("#edit").attr("style")
+
+            if (style !== undefined) {                
+                let filters = style.split(":")[1].split(";")[0].trim()
+                let regex = new RegExp(`${prop}\\([0-9]+(\\.[0-9]{1,3})?(%|px|deg)?\\)`)
+
+                if(filters.includes(prop)) {
+                    // $slide is slide of the btn which is clicked
+                    // setting the slider value and its span text to zero
+                    // after user turns off the switch
+                    $slide.val("0")
+                    $slide.prev().find(".range__num").text("0")
+
+                    // reassigning respective design variables to "" to cancel their effect
+                    // otherwise it will remain like blur(0)
+                    // prop = current filter i.e. being switched off
+
+                    switch (prop) {
+                        case "blur":
+                            star.blur = ""
+                            break;
+                        case "brightness":
+                            star.brightness = ""
+                            break;
+                        case "contrast":
+                            star.contrast = ""
+                            break;
+                        case "grayscale":
+                            star.grayscale = ""
+                            break;
+                        case "hue-rotate":
+                            star.hue = ""
+                            break;
+                        case "invert":
+                            star.invert = ""
+                            break;
+                        case "opacity":
+                            star.opacity = ""
+                            break;
+                        case "saturate":
+                            star.saturate = ""
+                            break;
+                        case "sepia":
+                            star.sepia = ""
+                            break;
+                    }
+
+                    let design = allStyles();
+                    design = design.replace(regex, "").trim();
+
+                    $("#edit").css("filter", design);
+
+                } else {
+                    console.log("Doesnt exist!")
+                }
+            } else {
+                console.log("You're not styled baby!")
+            }
+        }
+    })
+})
+
