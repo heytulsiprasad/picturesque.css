000bfa938f6a9a4c629ab6f422750963a22b4239
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-05-25 08:54:54 +0530

    :bug: fix image reset on changing viewport

--- src/layout.js
+++ src/layout.js
@@ -1,33 +1,35 @@
 // We will only show one of the images (which is being edited), below a
 // particular breakpoint i.e. $bp-largest
 
-let edited = `
+function edited() {
+  return `
     <div class="picture__box">
-        <img id="edit" class="picture__img" src="images/person-on-a-bridge-near-a-lake-747964.jpg"
-            alt="person-on-a-bridge" />
+        ${$(".picture__box").html().trim()}
     </div >
 `;
+}
 
-let both = `
+function both() {
+  return `
     <div class="picture__box">
-        <img id="edit" class="picture__img" src="images/person-on-a-bridge-near-a-lake-747964.jpg"
-            alt="person-on-a-bridge" />
+        ${$(".picture__box").html().trim()}
     </div>
     <div class="picture__box">
-      <img id="original" class="picture__img" src="images/person-on-a-bridge-near-a-lake-747964.jpg"
+      <img id="original" class="picture__img" src="https://picsum.photos/1280/808"
         alt="person-on-a-bridge" />
     </div>
 `;
+}
 
-function myFunction(x) {
+function changeImage(x) {
   if (x.matches) {
     // If media query matches
-    $(".picture").html(edited);
+    $(".picture").html(edited());
   } else {
-    $(".picture").html(both);
+    $(".picture").html(both());
   }
 }
 
-var x = window.matchMedia("(max-width: 1300px)");
-myFunction(x); // Call listener function at run time
-x.addListener(myFunction); // Attach listener function on state changes
+let query = window.matchMedia("(max-width: 1300px)");
+changeImage(query); // Call listener function at run time
+query.addListener(changeImage); // Attach listener function on state changes
